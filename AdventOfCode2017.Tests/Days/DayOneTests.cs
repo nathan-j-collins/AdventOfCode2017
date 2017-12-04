9b12649863e01a5631b04adc54f12203ba7669fa
Nathan Collins <nathan.collins@autologic.com>
2017-12-04 10:13:17 +0000

    first commit

---
+++ AdventOfCode2017.Tests/Days/DayOneTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode2017.Days;
+using System.Collections.Generic;
+
+namespace AdventOfCode2017.Tests.Days
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataTestMethod]
+        [DataRow("112233", 6)]
+        [DataRow("112231", 4)]
+        [DataRow("9879911229", 21)]
+        public void PartOneReturnsCorrectly(string input, int expected)
+        {
+            var result = DayOne.PartOne(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [DataTestMethod]
+        [DataRow("1212", 6)]
+        [DataRow("112233", 0)]
+        [DataRow("123456123456", 42)]
+        public void PartTwoReturnsCorrectly(string input, int expected)
+        {
+            var result = DayOne.PartTwo(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ AdventOfCode2017/Days/DayOne.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2017.Days
+{
+    public class DayOne
+    {
+        public static int PartOne(string input)
+        {
+            int result = 0;
+            int.TryParse(input.Substring(0, 1), out int firstChar);
+            int previousChar = firstChar;
+            for (int i = 1; i < input.Length; i++)
+            {
+                int.TryParse(input.Substring(i, 1), out int currentChar);
+
+                if (currentChar == previousChar)
+                {
+                    result += currentChar;
+                }
+                previousChar = currentChar;
+            }
+
+            if (previousChar == firstChar)
+                result += previousChar;
+
+            return result;
+        }
+
+        public static int PartTwo(string input)
+        {
+            int result = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int.TryParse(input.Substring(i, 1), out int currentChar);
+
+                int nextIndex = i + (input.Length / 2);
+                if (nextIndex >= input.Length)
+                    nextIndex = i - (input.Length / 2);
+
+                int.TryParse(input.Substring(nextIndex, 1), out int nextChar);
+
+                if (currentChar == nextChar)
+                {
+                    result += currentChar;
+                }
+            }
+
+            return result;
+        }
+    }
+}
